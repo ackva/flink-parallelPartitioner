209810e441c5e673e0efc81538e98780b7a91ff6
Ackva <ackva@users.noreply.github.com>
2019-05-06 19:27:23 +0200

    BETA - sort of clean DRAFT for windowed processing

--- src/main/java/org/myorg/quickstart/sharedState/PhasePartitioner.java
+++ src/main/java/org/myorg/quickstart/sharedState/PhasePartitioner.java
@@ -11,12 +11,15 @@ import org.apache.flink.api.java.typeutils.TupleTypeInfo;
 import org.apache.flink.streaming.api.TimeCharacteristic;
 import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.WindowedStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.api.windowing.triggers.CountTrigger;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class PhasePartitioner {
 
@@ -43,11 +46,15 @@ public class PhasePartitioner {
         // MapState Descriptor (as from data artisans)
         MapStateDescriptor<String, Tuple2<Integer, ArrayList<Integer>>> rulesStateDescriptor = new MapStateDescriptor<>("RulesBroadcastState", BasicTypeInfo.STRING_TYPE_INFO,tupleTypeInfo);
 
-        // Generate a new synthetic of edges
+        // Generate a new synthetic graph of edges
         TestingGraph edgeGraph = new TestingGraph("byOrigin",graphSize);
         edgeGraph.printGraph();
+
+        // Create initial EdgeStream
         DataStream<EdgeEvent> edgeSteam = edgeGraph.getEdgeStream(env);
 
+        // *** PHASE 1 ***
+        // Process edges to build the local model
         DataStream<HashMap> phaseOneStream = edgeSteam
                 .keyBy(new KeySelector<EdgeEvent, Integer>() {
                     @Override
@@ -60,6 +67,7 @@ public class PhasePartitioner {
                 });
         //phaseOneStream.print();
 
+        // Process edges in the similar time windows to "wait" for phase 2
         DataStream<EdgeEvent> edgesWindowed = edgeSteam
                 .keyBy(new KeySelector<EdgeEvent, Integer>() {
                     @Override
@@ -71,13 +79,13 @@ public class PhasePartitioner {
                 //.trigger(CountTrigger.of(1))
                 .process(new ProcessWindow() {
                 });
-        //phaseOneStream.print();
 
-        MatchFunctionEdges matchRules = new MatchFunctionEdges();
-
+        // *** Phase 2 ***
+        // Broadcast local state from Phase 1 to all Task Managers
         BroadcastStream<HashMap> broadcastFrequency = phaseOneStream
                 .broadcast(rulesStateDescriptor);
 
+        // Connect Broadcast Stream and Edge Stream to build global model
         DataStream<Tuple2<EdgeEvent,Integer>> phaseTwoStream = edgesWindowed
                 .keyBy(new KeySelector<EdgeEvent, Integer>() {
                     @Override
@@ -86,18 +94,21 @@ public class PhasePartitioner {
                     }
                 })
                 .connect(broadcastFrequency)
-                .process(matchRules);
+                .process(new MatchFunctionEdges());
 
-        //phaseTwoStream.print();
-
-
-
-        //Print result as Tuple3 (Vertex, Vertex, Partition) --> e.g. (4,2,0) is Edge(4,2) located in Parttition 0
+        //Print result in human-readable way
+             // Tuple3 (Vertex, Vertex, Partition) --> e.g. (4,2,0) is Edge(4,2) located in Parttition 0
         phaseTwoStream.map(new MapFunction<Tuple2<EdgeEvent, Integer>, Tuple3<Integer, Integer, Integer>>() {
             public Tuple3<Integer, Integer, Integer> map(Tuple2<EdgeEvent, Integer> input) {
                 return new Tuple3<>(input.f0.getEdge().getOriginVertex(), input.f0.getEdge().getDestinVertex(), input.f1);
             }
         }).print();
+
+
+
+        // *** Job Analytics
+
+
 
         // ### Finally, execute the job in Flink
         env.execute();
